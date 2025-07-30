a230c0be18f9532796a96f92380924dd34422c30
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-07-30 17:42:11 -0400

    gug

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use wgpu::*;
+
+fn main() {
+    let instance_descriptor = InstanceDescriptor::from_env_or_default();
+    let instance = Instance::new(&instance_descriptor);
+
+    let adapter_request_options
+}
